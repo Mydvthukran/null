d4be4b44bfe70e2535d00cb7cdd18d0e957a1297
Manish Yadav <my3596418@gmail.com>
2026-06-17 00:13:18 +0530

    feat: initialize navigation menu configuration with site-wide structure

--- src/constants/navConfig.js
+++ src/constants/navConfig.js
@@ -89,6 +89,7 @@ export const navItems = [
       { name: 'Syllabus', href: '/academics/syllabus' },
       { name: 'Admission Form', href: '/admission-form' },
       { name: 'Admission Prospectus', href: '/academics/admission-prospectus' },
+      { name: 'Pay Fees Online', href: '/pay-fees-online' },
       { name: 'Exam Schedule', href: '/academics/exam-schedule' },
       { name: 'Grievance Portal', href: 'https://grievance.sietpanchkula.ac.in/', external: true },
       { name: 'Code of Conduct', href: '/academics/code-of-conduct' },
@@ -142,6 +143,7 @@ export const navItems = [
     href: '/admission-helpline',
     submenu: [
       { name: 'Admission Helpline', href: '/admission-helpline' },
+      { name: 'Admission Prospectus', href: '/academics/admission-prospectus' },
       { name: 'Admission Documents', href: '/admission-documents' },
       { name: 'Pay Fees Online', href: '/pay-fees-online' }
     ]
