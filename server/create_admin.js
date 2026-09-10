f87cbed939c72741031d419ccf2d7a3120a15d29
Manish Yadav <my3596418@gmail.com>
2026-09-10 18:24:25 +0530

    feat: add scripts to create and reset the super admin account credentials

---
+++ server/create_admin.js
@@ -0,0 +1,19 @@
+﻿const pool = require('./config/db');
+const bcrypt = require('bcryptjs');
+
+(async () => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM admins WHERE username = "siet_admin"');
+    if (rows.length === 0) {
+      const hash = await bcrypt.hash('siet@2025', 10);
+      await pool.query('INSERT INTO admins (username, password, name, role) VALUES (?, ?, ?, ?)', ['siet_admin', hash, 'Super Admin', 'super_admin']);
+      console.log('Created siet_admin');
+    } else {
+      console.log('siet_admin already exists');
+    }
+    process.exit(0);
+  } catch(err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
---
+++ server/reset_admin.js
@@ -0,0 +1,14 @@
+﻿const pool = require('./config/db');
+const bcrypt = require('bcryptjs');
+
+(async () => {
+  try {
+    const hash = await bcrypt.hash('siet@2025', 10);
+    await pool.query('UPDATE admins SET password = ? WHERE username = "siet_admin"', [hash]);
+    console.log('Password reset for siet_admin');
+    process.exit(0);
+  } catch(err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
